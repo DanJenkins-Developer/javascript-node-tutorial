aea3e6610047c34604a6e20c9929ce71a1a95afd
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-01-15 17:01:08 -0600

    app.js

--- node-express-course/03-task-manager/starter/app.js
+++ node-express-course/03-task-manager/starter/app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require("express")
+const app = express()
+
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.send('Hello world')
+})
+
+app.get('/api/v1/tasks')
+
+// app.get('/api/v1/tasks')         - get all the tasks
+// app.post('api/v1/tasks')         - create a new task
+// app.get('/api/v1/tasks/:id')     - get single task
+// api.patch('/api/v1/tasks/:id')   - update task
+// app.delete('/api/v1/tasks/:id')  - delete task
+
+app.listen(port, console.log(`server is listening on port ${port}`))
