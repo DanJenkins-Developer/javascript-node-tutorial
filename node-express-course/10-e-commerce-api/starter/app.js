225d393ddb92dd82ad035a849fc2abb860389de4
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-01-23 09:23:52 -0600

    Started server and connected to DB

--- node-express-course/10-e-commerce-api/starter/app.js
+++ node-express-course/10-e-commerce-api/starter/app.js
@@ -1,1 +1,20 @@
-console.log('E-Commerce API');
+require('dotenv').config()
+// express
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+const PORT = process.env.PORT || 5000
+
+console.log(process.env.MONGO_URI);
+
+const start = async () => {
+    try {
+        connectDB(process.env.MONGO_URI)
+        app.listen(PORT)
+        console.log(`Server is listening on port ${PORT}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
