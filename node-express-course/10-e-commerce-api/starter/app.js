0e8251d951fbbe72131cbb4da5b80621cfdf25c6
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-01-23 10:02:46 -0600

    Finished basic app set up

--- node-express-course/10-e-commerce-api/starter/app.js
+++ node-express-course/10-e-commerce-api/starter/app.js
@@ -1,15 +1,38 @@
 // environment variables 
 require('dotenv').config()
+
+// async errors package
+require('express-async-errors')     // Used so we don't have to put try/catch blocks in each of our controllers
+
 // express
 const express = require('express')
 const app = express()
+
+// More packages
+const morgan = require('morgan')
+
 // database
 const connectDB = require('./db/connect')
 const PORT = process.env.PORT || 5000
 
+// middleware imports
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+// Global middleware. These get hit before the routes
+app.use(morgan('tiny'))     // Logging data/Debugging
+app.use(express.json())     // Grants access to JSON data in req.body
+
+
+// routes
 app.get('/', (req, res) => {
-    
+    res.send('E-Commerce API')
 })
+
+// error middleware
+app.use(notFoundMiddleware)     // Middleware gets hit if no routes are found. Invoked outside of the routes.
+app.use(errorHandlerMiddleware) // Middle ware gets hit if there is an error inside the route. Invoked inside. 
+
 
 const start = async () => {
     try {
